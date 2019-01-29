9e1dc252e8b3a0efad32dd3561ae7a24053277f6
Kyohei Ogasawara <r4586mlk@gmail.com>
2019-01-29 14:40:07 +0900

    Add flow-typed

--- .eslintrc.js
+++ .eslintrc.js
@@ -100,6 +100,9 @@ module.exports = {
   },
 
   settings: {
+    flowtype: {
+      onlyFilesWithFlowAnnotation: true,
+    },
     // Allow absolute paths in imports, e.g. import Button from 'components/Button'
     // https://github.com/benmosher/eslint-plugin-import/tree/master/resolvers
     'import/resolver': {
